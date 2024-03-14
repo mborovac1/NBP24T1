517b809165b51c61fb2cc755c8e56d460d2ba440
Muhamed Borovac <mborovac1@etf.unsa.ba>
2024-03-14 23:03:31 +0100

    Initial commit

---
+++ src/main/java/ba/team1/ads_project/Main.java
@@ -0,0 +1,13 @@
+package ba.team1.ads_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
